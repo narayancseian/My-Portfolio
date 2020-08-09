f343dd910d0480bc5661091416b54368dc9c265c
Narayan kumar <57906682+narayancseian@users.noreply.github.com>
2020-08-09 20:57:43 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,20 +1,23 @@
 <?php
-	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
-        $email=$_POST['email'];
-        $subject=$_POST['subject'];
-		$message=$_POST['message'];
-
-		$to_email="narayankr09072000@gmail.com"; 
-		$subject="Message from Portfolio";
-		$email_body="Name :$name.\n".
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email']
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message']
+$email_body="Name :$name.\n".
  
-"email :$email.\n".
+"Email :$email.\n".
 "Subject :$subject.\n".
-"message :$message.\n";
-		$headers="From: $email.\r\n";
-		mail($to_email, $subject, $email_body, $headers);
-		header("Location: index.php?mailsend");
-		
-	}
+"Message :$message.\n";
+
+if (empty($name || empty($email) || empty($subject) || empty($message))
+{
+    echo "Please fill all the fields";
+}
+else
+{
+    mail("narayankr09072000@gmail.com", "Message from Portfolio", $email_body);
+    echo "<script type='text/javascript'>alert('your message sent successfully');
+         window.history.log(-1);
+         </script>";
+}
 ?>
